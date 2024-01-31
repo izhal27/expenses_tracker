104f803d387e8ab9d5692179f282e70d93d1277f
Risal Walangadi <risal.walangadi@gmail.com>
2024-01-31 18:48:04 +0800

    add expenses widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Expenses());
+  runApp(const MaterialApp(
+    home: Expenses(),
+  ));
 }
