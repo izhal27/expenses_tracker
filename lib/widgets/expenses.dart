623ef80e1fc7241fd2503157cbe5b9f087c8dfa7
Risal Walangadi <risal.walangadi@gmail.com>
2024-02-09 17:32:33 +0800

    add chart bar

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -107,7 +108,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('Chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
